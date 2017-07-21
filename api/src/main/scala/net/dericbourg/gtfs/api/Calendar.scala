6443a8df6bfd622d6c6a44635d38cd918409eca4
Alban Dericbourg <alban@dericbourg.net>
2017-07-21 13:39:47 +0200

    Add missing Calendar doc
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- api/src/main/scala/net/dericbourg/gtfs/api/Calendar.scala
+++ api/src/main/scala/net/dericbourg/gtfs/api/Calendar.scala
@@ -3,6 +3,8 @@ package net.dericbourg.gtfs.api
 import java.time.{DayOfWeek, LocalDate}
 
 /**
+  * Services define a range of dates between which a Trip is available, the days of the week when it is available (such
+  * as Monday through Friday). A single Service can be applied to multiple different Trips.
   *
   * @see [[https://developers.google.com/transit/gtfs/reference/calendar-file GTFS format API doc]]
   * @param serviceId      Uniquely identifies a set of dates when service is available for one or more routes (from
